81182ad5bfd326db8975b64d4d67de7840e4b0b1
Nicholas Bulfon <pendragon177@hotmail.com>
2018-02-22 15:59:04 -0500

    Start of Curtain Climbing
    Script is mostly done. Still need to do bug fixes, correct assets and change some features to match intended design.

--- Assets/Scripts/Player/Puss In Boots/PussInBoots.cs
+++ Assets/Scripts/Player/Puss In Boots/PussInBoots.cs
@@ -22,8 +22,9 @@ public class PussInBoots : Cat {
 	public bool startedParryStance;
 	public bool parryStanceActivated;
 	public bool isParrying;
+    public bool isClimbing;//Nick
 
-	public Enemy enemyBeingParried;
+    public Enemy enemyBeingParried;
 
 	void Awake(){
 		rightAttackingPoint.enabled = false;
@@ -39,9 +40,32 @@ public class PussInBoots : Cat {
 		myRigidBody2D = GetComponent<Rigidbody2D>();
 		myRigidBody2D.velocity = new Vector2(myRigidBody2D.velocity.x,0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+    //Nick
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Climbable")
+        {
+            //Debug.Log("This worked. "+ isClimbing);
+            isClimbing = true;
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+            myRigidBody2D.velocity = new Vector3(0, 0, 0);
+            
+        }
+    }
+    //Nick
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Climbable")
+        {
+            //Debug.Log("This worked. " + isClimbing);
+            isClimbing = false;
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+            isJumping = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 
 		if(Input.GetButtonDown(returnToHubGamepadButton) || Input.GetKeyDown(returnToHubKey)){
 			ReturnToHub();
@@ -63,8 +87,22 @@ public class PussInBoots : Cat {
 				} else {
 					Idle();
 				}
+                //Nick
+                if (isClimbing)
+                {
+                    if (Input.GetButton("Vertical") && Input.GetAxisRaw("Vertical") > 0 || (Input.GetAxis("Vertical") >= 0.5f))
+                    {
+                        myRigidBody2D.transform.position += Vector3.up * 2 * Time.deltaTime;
+                    }
 
-				if(Input.GetKeyDown (jumpKey) || Input.GetButtonDown(jumpGamepadButton)){
+                    if (Input.GetButton("Vertical") && Input.GetAxisRaw("Vertical") < 0 || (Input.GetAxis("Vertical") <= -0.5f))
+                    {
+                        myRigidBody2D.transform.position += -Vector3.up * 2 * Time.deltaTime;
+                        Debug.Log(Input.GetAxis("Vertical"));
+                    }
+                }
+
+                if (Input.GetKeyDown (jumpKey) || Input.GetButtonDown(jumpGamepadButton)){
 
 					if(!isFalling){
 
