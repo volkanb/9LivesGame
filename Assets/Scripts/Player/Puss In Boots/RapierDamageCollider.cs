13419f0d78e7a8912feba733d0775d1c45f30e27
Nicholas Bulfon <pendragon177@hotmail.com>
2018-03-15 01:19:09 -0400

    ChandelierFall Progress
    Placed temp anchors and a giant chandelier to fall. Current just falls when the anchors are destroyed.

--- Assets/Scripts/Player/Puss In Boots/RapierDamageCollider.cs
+++ Assets/Scripts/Player/Puss In Boots/RapierDamageCollider.cs
@@ -3,23 +3,41 @@ using System.Collections;
 
 public class RapierDamageCollider : MonoBehaviour {
 
-	void OnTriggerEnter2D(Collider2D other){
- 
-		if(other.gameObject.tag == "Enemy"){
+    public int cuts = 0;
 
-			Enemy enemyVariables = other.GetComponent<Enemy> ();
+    void OnTriggerEnter2D(Collider2D other)
+    {
 
-			if(enemyVariables.canReceiveDamage){
-				if(!enemyVariables.invulnerable ){
-					
-					enemyVariables.life -= 1;
-					enemyVariables.receivedDamage = true;
-					 
-				}
-			} else{
-				PussInBoots player = FindObjectOfType<PussInBoots>();
+        if (other.gameObject.tag == "Enemy")
+        {
 
-			}
-		}
-	}
+            Enemy enemyVariables = other.GetComponent<Enemy>();
+
+            if (enemyVariables.canReceiveDamage)
+            {
+                if (!enemyVariables.invulnerable)
+                {
+
+                    enemyVariables.life -= 1;
+                    enemyVariables.receivedDamage = true;
+
+                }
+            }
+            else
+            {
+                PussInBoots player = FindObjectOfType<PussInBoots>();
+
+            }
+        }
+
+        if (other.gameObject.tag == "Anchor")
+        {
+
+            int chandeliersCut = FindObjectOfType<ChandelierFall>().anchorsCut;
+
+            chandeliersCut++;
+            //cuts += 1;
+            Destroy(other.gameObject);
+        }
+    }
 }
